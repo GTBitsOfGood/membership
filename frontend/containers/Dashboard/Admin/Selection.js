6030115f030b3f9ad4cd704868bee2fd767862a6
Oberon <ahijaouy@gatech.edu>
2017-12-27 22:23:59 +0000

    basic admin flow/navigation established

---
+++ frontend/containers/Dashboard/Admin/Selection.js
@@ -0,0 +1,38 @@
+// NPM Imports
+import propTypes from "prop-types";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+
+// Local Imports
+// import Dashboard from "./Dashboard";
+// import Splash from "../components/Splash";
+// import { login, logout } from "../ducks/auth";
+// import PrivateRoute from "../components/PrivateRoute";
+
+class Selection extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>Selection Kanban</h1>
+      </div>
+    );
+  }
+}
+
+Selection.propTypes = {};
+
+function mapStateToProps(state) {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Selection);
--- frontend/containers/Dashboard/Admin/index.js
+++ frontend/containers/Dashboard/Admin/index.js
@@ -2,10 +2,13 @@
 import { Layout, Menu, Row, Col, Card, InputNumber } from "antd";
 import propTypes from "prop-types";
 import React from "react";
+import { Route, Switch, Link} from "react-router-dom";
 
 // Local Imports & Constants
-import Application from "../../../components/Application";
-import { ToggleButton } from "react-bootstrap";
+import Applicants from "./Applicants";
+import Home from "./Home";
+import Selection from "./Selection";
+import Projects from "./Projects";
 const { Header, Content, Footer } = Layout;
 
 const Admin = ({ logout, user }) => {
@@ -15,34 +18,42 @@ const Admin = ({ logout, user }) => {
     }
   };
 
-  return (
-    <Layout className="layout">
+  return (<Layout className="layout">
       <Header>
         <div className="menu-title">GT Bits of Good</div>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["home"]}
-          style={{ lineHeight: "64px" }}
-          onClick={navigate}
-        >
-          <Menu.Item key="home">Home</Menu.Item>
-          <Menu.Item key="applicants">Applicants</Menu.Item>
-          <Menu.Item key="projects">Projects</Menu.Item>
-          <Menu.Item key="selection">Selection</Menu.Item>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["home"]} style={{ lineHeight: "64px" }} onClick={navigate}>
+          <Menu.Item key="home">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="applicants">
+            <Link to="/applicants">Applicants</Link>
+          </Menu.Item>
+          <Menu.Item key="projects">
+            <Link to="/projects">Projects</Link>
+          </Menu.Item>
+          <Menu.Item key="selection">
+            <Link to="/selection">Selection</Link>
+          </Menu.Item>
           <Menu.Item key="logout">Logout</Menu.Item>
         </Menu>
       </Header>
       <Content className="content-container">
         <div className="content">
-          <h1>Admin Dash</h1>
+          <Switch>
+            <Route exact path="/applicants/:id" component={Applicants} />
+            <Route exact path="/projects/:id" component={Projects} />
+            <Route exact path="/selection/:id" component={Selection} />
+            <Route exact path="/applicants" component={Applicants} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/selection" component={Selection} />
+            <Route exact path="/" component={Home} />
+          </Switch>
         </div>
       </Content>
       <Footer className="center">
         Made with &#9829; by Bits of Good ©2018
       </Footer>
-    </Layout>
-  );
+    </Layout>);
 };
 
 Admin.propTypes = {
