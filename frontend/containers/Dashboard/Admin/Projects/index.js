9886bb5015a8cbb267bdcbb984506483ac64cd5c
Oberon <ahijaouy@gatech.edu>
2017-12-29 10:25:33 +0000

    replaced react-table on project page with antd table

--- frontend/containers/Dashboard/Admin/Projects/index.js
+++ frontend/containers/Dashboard/Admin/Projects/index.js
@@ -1,64 +1,130 @@
 import React from "react";
 import propTypes from "prop-types";
-import ReactTable from "react-table";
-import { Icon, Button, Avatar} from 'antd';
+import {
+  Icon,
+  Button,
+  Avatar,
+  Table,
+  Divider
+} from 'antd';
+
 
 import "react-table/react-table.css";
-
-const columns = [
+const columns = (updateCurrentProject) => [
   {
-    Header: "Organization",
-    accessor: "organization",
+    title: "Organization",
+    dataIndex: "organization",
+    render: (text, record ) => <a onClick={() => updateCurrentProject(record._id)}> {text} </a>
   },
   {
-    Header: "Contact Name",
-    accessor: "contact.name"
+    title: "Contact Name",
+    dataIndex: "contact.name"
   },
   {
-    Header: "Contact Phone",
-    accessor: "contact.phone"
+    title: "Contact Phone",
+    dataIndex: "contact.phone"
   },
   {
-    Header: "Contact Email",
-    accessor: "contact.email"
+    title: "Contact Email",
+    dataIndex: "contact.email"
   },
   {
-    Header: "Project Manager",
-    accessor: "project_manager.name"
+    title: "Project Manager",
+    dataIndex: "project_manager.name"
   },
   {
-    Header: "Team Members",
-    id: "project_members",
-    accessor: d => d.project_members.length
+    title: "Team Members",
+    dataIndex: "project_members",
+    render: item => <span style={{ textAlign: "center" }}> {item.length} </span>
   }
 ];
+// const columns = [{
+//   title: 'Organization',
+//   dataIndex: 'organization',
+//   render: text => ( <a onClick={() => alert('clicked title')}> {text} </a>),
+// },
+// {
+//   title: 'Contact Name',
+//   dataIndex: 'contact.name',
+// },
+// {
+//   title: 'Contact Phone',
+//   dataIndex: 'contact.phone',
+// },
+// {
+//   title: 'Contact Email',
+//   dataIndex: 'contact.email',
+// },
+// {
+//   title: 'Project Manager',
+//   dataIndex: 'project_manager.name',
+// },
+// {
+//   title: 'Team Members',
+//   dataIndex: 'project_members',
+//   render: item => ( <span style={{textAlign: "center"}} > {
+//       item.length
+//     } </span>),
 
-function pagination(data) {
-  return data.length > 10;
-}
-const Table = ({ data, update}) => (
+// },
+
+// ];
+
+const Projects = ({ data, update}) => (
   <div>
     <h1 className="center">Bits of Good Projects</h1>
-    <ReactTable
-      data={data}
-      columns={columns}
-      defaultPageSize={10}
-      showPageSizeOptions={false}
-      showPagination={pagination(data)}
-      className="-striped -highlight"
-      getTdProps={(state, rowInfo, column, instance) => {
-        return {
-          onClick: e => update(rowInfo.original._id)
-        };
-      }}
-    />
+    <Table columns = {columns(update)} dataSource = {data} />
   </div>
 );
 
-Table.propTypes = {
-  dataSet: propTypes.array,
+Projects.propTypes = {
+  data: propTypes.array,
   update: propTypes.func,
 
 };
 
-export default Table;
+export default Projects;
+
+
+// OLD SHIT
+
+
+// const columns = [{
+//   title: 'Name',
+//   dataIndex: 'name',
+//   key: 'name',
+//   render: text => (< a href = "#" > {
+//     text
+//   } < /a>),
+// }, {
+//   title: 'Age',
+//   dataIndex: 'age',
+//   key: 'age',
+// }, {
+//   title: 'Address',
+//   dataIndex: 'address',
+//   key: 'address',
+// }, {
+//   title: 'Action',
+//   key: 'action',
+//   render: (text, record) => ( <
+//     span >
+//     <
+//     a href = "#" > Action 一 {
+//       record.name
+//     } < /a> <
+//     Divider type = "vertical" / >
+//     <
+//     a href = "#" > Delete < /a> <
+//     Divider type = "vertical" / >
+//     <
+//     a href = "#"
+//     className = "ant-dropdown-link" >
+//     More actions < Icon type = "down" / >
+//     <
+//     /a> <
+//     /span>
+//   ),
+// }];
+
+
