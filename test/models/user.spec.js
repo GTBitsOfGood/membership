753c4d7b967d2f8904ca1e64903e10d9ff739067
Oberon <ahijaouy@gatech.edu>
2017-12-30 14:29:53 -0500

    remove test for user.email

--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -1,29 +1,29 @@
 var expect = require('chai').expect;
- 
+
 var User = require('../../backend/models/user');
- 
+
 describe('Model - User', function() {
     it('should be invalid if name is empty', function(done) {
         var m = new User();
- 
+
         m.validate(function(err) {
             expect(err.errors.name).to.exist;
             done();
         });
     });
-    it('should be invalid if email is empty', function(done) {
-        var m = new User();
-    
-        m.validate(function(err) {
-            expect(err.errors.email).to.exist;
-            done();
-        });
-    });
+    // it('should be invalid if email is empty', function(done) {
+    //     var m = new User();
+
+    //     m.validate(function(err) {
+    //         expect(err.errors.email).to.exist;
+    //         done();
+    //     });
+    // });
     it('should be invalid if score is not a number', function(done) {
         var m = new User({
             score: 'TEST'
         });
-    
+
         m.validate(function(err) {
             expect(err.errors.score).to.exist;
             done();
@@ -33,7 +33,7 @@ describe('Model - User', function() {
         var m = new User({
             name: {}
         });
-    
+
         m.validate(function(err) {
             expect(err.errors.name).to.exist;
             done();
@@ -43,7 +43,7 @@ describe('Model - User', function() {
         var m = new User({
             email: {}
         });
-    
+
         m.validate(function(err) {
             expect(err.errors.email).to.exist;
             done();
@@ -53,7 +53,7 @@ describe('Model - User', function() {
         var m = new User({
             credit_hours: 'TEST'
         });
-    
+
         m.validate(function(err) {
             expect(err.errors.credit_hours).to.exist;
             done();
@@ -63,7 +63,7 @@ describe('Model - User', function() {
         var m = new User({
             websites: {}
         });
-    
+
         m.validate(function(err) {
             expect(err.errors.websites).to.exist;
             done();
@@ -73,7 +73,7 @@ describe('Model - User', function() {
         var m = new User({
             websites: [{}, {}]
         });
-    
+
         m.validate(function(err) {
             expect(err.errors.websites).to.exist;
             done();
@@ -83,7 +83,7 @@ describe('Model - User', function() {
         var m = new User({
             role: 'Mr. Peanut Butter'
         });
- 
+
         m.validate(function(err) {
             expect(err.errors.role).to.exist;
             done();
@@ -91,7 +91,7 @@ describe('Model - User', function() {
     });
     it('should be invalid if github_id is empty', function(done) {
         var m = new User();
- 
+
         m.validate(function(err) {
             expect(err.errors.github_id).to.exist;
             done();
@@ -99,7 +99,7 @@ describe('Model - User', function() {
     });
     it('should be invalid if github_username is empty', function(done) {
         var m = new User();
- 
+
         m.validate(function(err) {
             expect(err.errors.github_username).to.exist;
             done();
@@ -107,7 +107,7 @@ describe('Model - User', function() {
     });
     it('should be invalid if github_access_token is empty', function(done) {
         var m = new User();
- 
+
         m.validate(function(err) {
             expect(err.errors.github_access_token).to.exist;
             done();
@@ -115,7 +115,7 @@ describe('Model - User', function() {
     });
     it('should be invalid if github_avatar_url is empty', function(done) {
         var m = new User();
- 
+
         m.validate(function(err) {
             expect(err.errors.github_avatar_url).to.exist;
             done();
@@ -123,7 +123,7 @@ describe('Model - User', function() {
     });
     it('should be invalid if github_profile_url is empty', function(done) {
         var m = new User();
- 
+
         m.validate(function(err) {
             expect(err.errors.github_profile_url).to.exist;
             done();
@@ -131,7 +131,7 @@ describe('Model - User', function() {
     });
     it('should be invalid if github_public_repos is empty', function(done) {
         var m = new User();
- 
+
         m.validate(function(err) {
             expect(err.errors.github_public_repos).to.exist;
             done();
@@ -139,7 +139,7 @@ describe('Model - User', function() {
     });
     it('should be invalid if github_followers is empty', function(done) {
         var m = new User();
- 
+
         m.validate(function(err) {
             expect(err.errors.github_followers).to.exist;
             done();
