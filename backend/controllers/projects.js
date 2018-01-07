03593cd9452a1def90a2eda73192ae4d9cd55d1f
Andre Hijaouy <hijaouya@gmail.com>
2018-01-07 00:18:41 -0800

    added prettier to codebase

--- backend/controllers/projects.js
+++ backend/controllers/projects.js
@@ -1,4 +1,4 @@
-const Project = require('mongoose').model('Project');
+const Project = require("mongoose").model("Project");
 
 module.exports.index = (req, res, next) => {
   // pagination settings
@@ -6,15 +6,21 @@ module.exports.index = (req, res, next) => {
   const skip = (req.query.page - 1 || 0) * limit;
 
   Promise.all([
-    Project.find().limit(limit).skip(skip).populate('project_manger').populate('project_members').exec(),
+    Project.find()
+      .limit(limit)
+      .skip(skip)
+      .populate("project_manger")
+      .populate("project_members")
+      .exec(),
     Project.count().exec()
-  ]).then(([projects, count]) => {
-    res.locals.data = {
-      projects,
-      count
-    };
-    return next();
-  })
+  ])
+    .then(([projects, count]) => {
+      res.locals.data = {
+        projects,
+        count
+      };
+      return next();
+    })
     .catch(err => {
       console.error(err);
       res.locals.errors = err;
@@ -23,9 +29,9 @@ module.exports.index = (req, res, next) => {
 };
 
 module.exports.get = (req, res, next) => {
-  Project.findById(req.params.id, )
-    .populate('project_manger')
-    .populate('project_members')
+  Project.findById(req.params.id)
+    .populate("project_manger")
+    .populate("project_members")
     .exec((err, project) => {
       if (err) {
         console.error(err);
@@ -41,45 +47,51 @@ module.exports.get = (req, res, next) => {
 };
 
 module.exports.store = (req, res, next) => {
-  Project.create({
-    name: req.body.name,
-    organization: req.body.organization,
-    contact: {
-      name: req.body.contact_name,
-      email: req.body.contact_email,
-      phone: req.body.contact_phone
+  Project.create(
+    {
+      name: req.body.name,
+      organization: req.body.organization,
+      contact: {
+        name: req.body.contact_name,
+        email: req.body.contact_email,
+        phone: req.body.contact_phone
+      },
+      project_manager: req.body.project_manager
     },
-    project_manager: req.body.project_manager
-  }, (err, project) => {
-    if (err) {
-      console.error(err);
-      res.locals.errors = err;
+    (err, project) => {
+      if (err) {
+        console.error(err);
+        res.locals.errors = err;
+        return next();
+      }
+
+      res.locals.data = {
+        project: project
+      };
       return next();
     }
-
-    res.locals.data = {
-      project: project
-    };
-    return next();
-  });
-}
+  );
+};
 
 module.exports.delete = (req, res, next) => {
-  Project.remove({
-    _id: req.params.id
-  }, (err, project) => {
-    if (err) {
-      console.error(err);
-      res.locals.errors = err;
+  Project.remove(
+    {
+      _id: req.params.id
+    },
+    (err, project) => {
+      if (err) {
+        console.error(err);
+        res.locals.errors = err;
+        return next();
+      }
+
+      res.locals.data = {
+        deleted: true
+      };
       return next();
     }
-
-    res.locals.data = {
-      deleted: true
-    };
-    return next();
-  });
-}
+  );
+};
 
 module.exports.members = (req, res, next) => {
   Project.findById(req.params.id, (err, project) => {
@@ -91,15 +103,11 @@ module.exports.members = (req, res, next) => {
 
     res.locals.data = {
       users: project.project_members
-    }
+    };
     return next();
   });
-}
+};
 
-module.exports.add_member = (req, res, next) => {
+module.exports.add_member = (req, res, next) => {};
 
-}
-
-module.exports.remove_member = (req, res, next) => {
-
-}
+module.exports.remove_member = (req, res, next) => {};
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,11 +1,11 @@
-const User = require('mongoose').model('User');
+const User = require("mongoose").model("User");
 
-const util = require('../services/util');
+const util = require("../services/util");
 
 module.exports.index = (req, res, next) => {
   if (req.query.count) {
     switch (req.query.count) {
-      case 'submitted': {
+      case "submitted": {
         User.count({ application_status: "submitted" }, (err, submitted) => {
           if (err) {
             console.log(err);
@@ -14,13 +14,13 @@ module.exports.index = (req, res, next) => {
           }
 
           res.locals.data = {
-            submitted,
+            submitted
           };
           return next();
         });
         break;
       }
-      case 'accepted': {
+      case "accepted": {
         User.count({ application_status: "accepted" }, (err, accepted) => {
           if (err) {
             console.log(err);
@@ -29,13 +29,13 @@ module.exports.index = (req, res, next) => {
           }
 
           res.locals.data = {
-            accepted,
+            accepted
           };
           return next();
         });
         break;
       }
-      case 'rejected': {
+      case "rejected": {
         User.count({ application_status: "rejected" }, (err, rejected) => {
           if (err) {
             console.log(err);
@@ -44,13 +44,13 @@ module.exports.index = (req, res, next) => {
           }
 
           res.locals.data = {
-            rejected,
+            rejected
           };
           return next();
         });
         break;
       }
-      case 'pm_interest': {
+      case "pm_interest": {
         User.count({ pm_interest: true }, (err, pmInterest) => {
           if (err) {
             console.log(err);
@@ -59,14 +59,14 @@ module.exports.index = (req, res, next) => {
           }
 
           res.locals.data = {
-            pm_interest: pmInterest,
+            pm_interest: pmInterest
           };
           return next();
         });
         break;
       }
       default: {
-        console.log('this should not be hit...');
+        console.log("this should not be hit...");
         console.log(req.query);
         return next();
       }
@@ -77,17 +77,24 @@ module.exports.index = (req, res, next) => {
     const skip = (req.query.page - 1 || 0) * limit;
 
     Promise.all([
-      User.find({ role: 'applicant' }).sort('-updatedAt')
-        .limit(limit).skip(skip).populate('languages').populate('databases')
-        .populate('web_technologies').populate('deployment').exec(),
-      User.count({ role: 'applicant' }).exec()
-    ]).then(([users, count]) => {
-      res.locals.data = {
-        users,
-        count
-      };
-      return next();
-    })
+      User.find({ role: "applicant" })
+        .sort("-updatedAt")
+        .limit(limit)
+        .skip(skip)
+        .populate("languages")
+        .populate("databases")
+        .populate("web_technologies")
+        .populate("deployment")
+        .exec(),
+      User.count({ role: "applicant" }).exec()
+    ])
+      .then(([users, count]) => {
+        res.locals.data = {
+          users,
+          count
+        };
+        return next();
+      })
       .catch(err => {
         console.error(err);
         res.locals.errors = err;
@@ -98,10 +105,10 @@ module.exports.index = (req, res, next) => {
 
 module.exports.get = (req, res, next) => {
   User.findById(req.params.id)
-    .populate('languages')
-    .populate('web_technologies')
-    .populate('databases')
-    .populate('deployment')
+    .populate("languages")
+    .populate("web_technologies")
+    .populate("databases")
+    .populate("deployment")
     .exec((err, usr) => {
       if (err) {
         console.error(err);
@@ -118,45 +125,54 @@ module.exports.get = (req, res, next) => {
 
 module.exports.store = (req, res, next) => {
   // protect application_status  and role changes
-  let appStatus = 'none';
-  let role = 'applicant';
+  let appStatus = "none";
+  let role = "applicant";
   if (req.user.role === "admin") {
-    appStatus = req.body.application_status ? req.body.application_status : appStatus;
+    appStatus = req.body.application_status
+      ? req.body.application_status
+      : appStatus;
     role = req.body.role ? req.body.role : role;
   }
 
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone,
-    title: req.body.title,
-    credit_hours: req.body.credit_hours,
-    frontend_experience: req.body.frontend_experience,
-    backend_experience: req.body.backend_experience,
-    pm_interest: req.body.pm_interest,
-    graduation_date: req.body.graduation_date ? req.body.graduation_date : null,
-    score: 0,
-    role: role,
-    application_status: appStatus,
-    websites: req.body.websites ? req.body.websites : [],
-    languages: req.body.languages ? req.body.languages : [],
-    web_technologies: req.body.web_technologies ? req.body.web_technologies : [],
-    databases: req.body.databases ? req.body.databases : [],
-    deployment: req.body.deployment ? req.body.deployment : [],
-    free_response: req.body.free_response ? req.body.free_response : {},
-    github: req.body.github ? req.body.github : {}
-  }, (err, usr) => {
-    if (err) {
-      console.error(err);
-      res.locals.error = err;
+  User.create(
+    {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      title: req.body.title,
+      credit_hours: req.body.credit_hours,
+      frontend_experience: req.body.frontend_experience,
+      backend_experience: req.body.backend_experience,
+      pm_interest: req.body.pm_interest,
+      graduation_date: req.body.graduation_date
+        ? req.body.graduation_date
+        : null,
+      score: 0,
+      role: role,
+      application_status: appStatus,
+      websites: req.body.websites ? req.body.websites : [],
+      languages: req.body.languages ? req.body.languages : [],
+      web_technologies: req.body.web_technologies
+        ? req.body.web_technologies
+        : [],
+      databases: req.body.databases ? req.body.databases : [],
+      deployment: req.body.deployment ? req.body.deployment : [],
+      free_response: req.body.free_response ? req.body.free_response : {},
+      github: req.body.github ? req.body.github : {}
+    },
+    (err, usr) => {
+      if (err) {
+        console.error(err);
+        res.locals.error = err;
+        return next();
+      }
+
+      res.locals.data = {
+        user: usr
+      };
       return next();
     }
-
-    res.locals.data = {
-      user: usr
-    };
-    return next();
-  });
+  );
 };
 
 module.exports.update = (req, res, next) => {
@@ -171,30 +187,54 @@ module.exports.update = (req, res, next) => {
     user.email = req.body.email ? req.body.email : user.email;
     user.phone = req.body.phone ? req.body.phone : user.phone;
     user.title = req.body.title ? req.body.title : user.title;
-    user.pm_interest = req.body.pm_interest ? req.body.pm_interest : user.pm_interest;
-    user.credit_hours = req.body.credit_hours ? req.body.credit_hours : user.credit_hours;
-    user.graduation_date = req.body.graduation_date ? req.body.graduation_date : user.graduation_date;
+    user.pm_interest = req.body.pm_interest
+      ? req.body.pm_interest
+      : user.pm_interest;
+    user.credit_hours = req.body.credit_hours
+      ? req.body.credit_hours
+      : user.credit_hours;
+    user.graduation_date = req.body.graduation_date
+      ? req.body.graduation_date
+      : user.graduation_date;
     user.role = req.body.role ? req.body.role : user.role;
-    user.frontend_experience = req.body.frontend_experience ? req.body.frontend_experience : user.frontend_experience;
-    user.backend_experience = req.body.backend_experience ? req.body.backend_experience : user.backend_experience;
-    user.free_response = req.body.free_response ? req.body.free_response : user.free_response;
+    user.frontend_experience = req.body.frontend_experience
+      ? req.body.frontend_experience
+      : user.frontend_experience;
+    user.backend_experience = req.body.backend_experience
+      ? req.body.backend_experience
+      : user.backend_experience;
+    user.free_response = req.body.free_response
+      ? req.body.free_response
+      : user.free_response;
     user.websites = req.body.websites ? req.body.websites : user.websites;
-    user.languages = req.body.languages ? await util.strsToLangs(req.body.languages, "languages") : [];
-    user.web_technologies = req.body.web_technologies ? await util.strsToLangs(req.body.web_technologies, "web_technologies") : [];
-    user.databases = req.body.databases ? await util.strsToLangs(req.body.databases, "databases") : [];
-    user.deployment = req.body.deployment ? await util.strsToLangs(req.body.deployment, "deployment") : [];
+    user.languages = req.body.languages
+      ? await util.strsToLangs(req.body.languages, "languages")
+      : [];
+    user.web_technologies = req.body.web_technologies
+      ? await util.strsToLangs(req.body.web_technologies, "web_technologies")
+      : [];
+    user.databases = req.body.databases
+      ? await util.strsToLangs(req.body.databases, "databases")
+      : [];
+    user.deployment = req.body.deployment
+      ? await util.strsToLangs(req.body.deployment, "deployment")
+      : [];
     user.github = req.body.github ? req.body.github : user.github;
     user.score = util.generateScore(user);
     // protect application_status changes
-    if ((req.body.application_status) && (req.body.application_status === "submitted" || req.user.role === "admin")) {
-      user.application_status = req.body.application_status ? req.body.application_status : user.application_status;
+    if (
+      req.body.application_status &&
+      (req.body.application_status === "submitted" || req.user.role === "admin")
+    ) {
+      user.application_status = req.body.application_status
+        ? req.body.application_status
+        : user.application_status;
     }
 
     // protect role changes
     if (req.body.role && req.user.role === "admin") {
       user.role = req.body.role ? req.body.role : user.role;
     }
-
 
     user.save((err, updated) => {
       if (err) {
@@ -212,17 +252,20 @@ module.exports.update = (req, res, next) => {
 };
 
 module.exports.delete = (req, res, next) => {
-  User.remove({
-    _id: req.params.id
-  }, (err, usr) => {
-    if (err) {
-      console.error(err);
-      res.locals.error = err;
+  User.remove(
+    {
+      _id: req.params.id
+    },
+    (err, usr) => {
+      if (err) {
+        console.error(err);
+        res.locals.error = err;
+        return next();
+      }
+      res.locals.data = {
+        deleted: true
+      };
       return next();
     }
-    res.locals.data = {
-      deleted: true
-    };
-    return next();
-  });
+  );
 };
